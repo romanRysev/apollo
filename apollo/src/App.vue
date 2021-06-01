a337226ea5ce6c7c92c9bc38f32f7e0c4aa1db58
Roman <r.rysev@tesonero.com>
2021-06-01 17:34:29 +0300

    базовая структура компонентов

---
+++ apollo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <ToDo />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ToDo from "./components/ToDo";
+
+export default {
+  name: "App",
+
+  components: {
+    ToDo,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ apollo/src/components/TaskList.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>{{ taskList.title }}</h1>
+    <p v-for="task in taskList.tasks" :key="task.id">{{ task.text }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: { taskList: { type: Object } },
+};
+</script>
+
+<style></style>
